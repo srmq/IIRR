f805af06442c05e96fda5525c84755df03081501
Sergio Queiroz <srmq@srmq.org>
2018-03-07 08:16:54 -0300

    Added LICENSE

--- IIRR.ino
+++ IIRR.ino
@@ -1,6 +1,20 @@
-/*
-  author: srmq@cin.ufpe.br
-*/
+/**
+ * IIRR -- Intelligent Irrigator Based on ESP8266
+    Copyright (C) 2016--2018  Sergio Queiroz <srmq@cin.ufpe.br>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
